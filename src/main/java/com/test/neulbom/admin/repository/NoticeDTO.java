53212055203511f9c61448b53d92b0e9e8ba8955
yeonwd <dev.yeonw@gmail.com>
2023-06-04 22:19:56 +0900

    게시판 관리 초기 작업

---
+++ src/main/java/com/test/neulbom/admin/repository/NoticeDTO.java
@@ -0,0 +1,14 @@
+package com.test.neulbom.admin.repository;
+
+import lombok.Data;
+
+@Data
+public class NoticeDTO {
+	
+	String notice_seq;
+	String title;
+	String content;
+	String notice_date;
+	String read;
+	
+}
