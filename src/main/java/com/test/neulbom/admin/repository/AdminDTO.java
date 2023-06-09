38b767df9a32659b78f71a45929c1a4eb67899cc
DongDongEEEE <cnsdh78@gmail.com>
2023-06-09 14:47:25 +0900

    희윤 동재 1447

--- src/main/java/com/test/neulbom/admin/repository/AdminDTO.java
+++ src/main/java/com/test/neulbom/admin/repository/AdminDTO.java
@@ -4,10 +4,7 @@ import lombok.Data;
 
 @Data
 public class AdminDTO {
-<<<<<<< HEAD
 	
-=======
->>>>>>> b14404054d7c38044ad09b440847598477274821
 	private String admin_seq;
 	private String id;
 	private String pw;
@@ -17,13 +14,10 @@ public class AdminDTO {
 	private String email;
 	private String pic;
 	private String lv;
-<<<<<<< HEAD
-=======
 	
 	private String bank;
 	private String bank_account;
 	private String salary;
 	
->>>>>>> b14404054d7c38044ad09b440847598477274821
 	
 }
