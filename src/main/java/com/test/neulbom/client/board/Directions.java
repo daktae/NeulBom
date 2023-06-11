9409eeeaaa5689180f24cab7fe330f8a83e33b42
TaeWan <xodhks0423@naver.com>
2023-06-12 00:44:08 +0900

    잠깐 커밋

--- src/main/java/com/test/neulbom/client/board/Directions.java
+++ src/main/java/com/test/neulbom/client/board/Directions.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/directions.do")
+@WebServlet("/client/board/directions.do")
 public class Directions extends HttpServlet {
 
 	@Override
